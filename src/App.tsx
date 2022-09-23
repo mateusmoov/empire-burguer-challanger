e5567ce88845b012cbb581d9988ac63977f449ac
Mateus Vasconcelos <mateusvasconcelos4210@gmail.com>
2022-09-23 00:25:42 -0300

    📦 NEW: Adding button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { useState } from 'react'
 import { globalStyles } from 'lib/globalStyles'
 import Text from 'components/Typography/TypographyText'
+import { StyledButton, StyledIconButton } from 'components/Button/Button'
+import WhatsAppIcon from 'assets/whatsapp-logo.svg'
 import './App.css'
 
 function App() {
   globalStyles()
   return (
     <div className="App">
-      <Text>
+      <StyledButton variant='secondary'>
+        HELLO WORLD
+      </StyledButton>
+
+      <StyledIconButton variant='primary'>
+        <img src={WhatsAppIcon} alt="zapzap" />
         Hello World
-      </Text>
-    </div>
+      </StyledIconButton>
+    </div >
   )
 }
 
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,66 @@
+import { styled } from 'lib/stitches.config'
+
+export const StyledButton = styled('button', {
+    //Reset and position
+    WebkitAppearance: 'none',
+    WebkitTapHighlightColor: 'transparent',
+    WebkitUserSelect: 'none',
+    MozUserSelect: 'none',
+    MsUserSelect: 'none',
+    userSelect: 'none',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexShrink: '0',
+    outline: 'none',
+    cursor: 'pointer',
+    border: '0',
+    font: 'inherit',
+    borderRadius: '0.25rem',
+    color: 'var(--palette-white)',
+    fontWeight: '700',
+
+    variants: {
+        variant: {
+            primary: {
+                backgroundColor: 'var(--palette-red)',
+            },
+            secondary: {
+                backgroundColor: 'var(--palette-bege)',
+                color: 'var(--palette-red)',
+                fontFamily: 'var(--font-display)',
+                fontWeight: 'var(--font-weight-4)'
+            }
+        }
+    }
+})
+
+export const StyledIconButton = styled('button', {
+    //Reset and position
+    WebkitAppearance: 'none',
+    WebkitTapHighlightColor: 'transparent',
+    WebkitUserSelect: 'none',
+    MozUserSelect: 'none',
+    MsUserSelect: 'none',
+    userSelect: 'none',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexShrink: '0',
+    outline: 'none',
+    cursor: 'pointer',
+    border: '0',
+    borderRadius: '0.25rem',
+    color: 'var(--palette-white)',
+    fontWeight: '700',
+    gap: 10,
+    variants: {
+        variant: {
+            primary: {
+                backgroundColor: 'var(--palette-yellow)',
+                color: 'var(--palette-brown-200)',
+                fontFamily: 'var(--font-display)',
+            },
+        }
+    }
+})
