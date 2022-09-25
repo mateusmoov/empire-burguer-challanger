6eacd3303c970ca987433222fee01c31694f654e
Mateus Vasconcelos <mateusvasconcelos4210@gmail.com>
2022-09-25 04:10:51 -0300

    📦 NEW: Adding Banner with a blur in the navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,42 +2,125 @@ import { Flex } from "components/Flex/Flex";
 import Text from "components/Typography/TypographyText";
 import Divider from "components/Divider/Divider";
 import Crown from "assets/crown-logo.svg";
-import IfoodLogo from "assets/ifood-logo.svg"
-import InstagramLogo from "assets/instagram-logo.svg"
-import WhatsAppLogo from 'assets/whatsapp-logo.svg'
+import IfoodLogo from "assets/ifood-logo.svg";
+import InstagramLogo from "assets/instagram-logo.svg";
+import WhatsAppLogo from "assets/whatsapp-logo.svg";
+import Banner1 from "assets/Banner01.png";
 import { StyledIconButton } from "components/Button/Button";
-
 
 export const Navbar = () => {
     return (
-        <Flex justifyContent={'center'} css={{ backgroundColor: 'red' }}>
-            <Flex css={{ gap: 7, marginRight: 61 }}
+        <>
+            <div
+                style={{
+                    backgroundImage: "url('src/assets/Banner01.png')",
+                    minHeight: "545px",
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                    position: "relative",
+                }}
             >
-                <img src={Crown} alt="CrownLogo" />
-                <Text family={"display"} weight={"4"} size={"7"} variant={"brown200"}>
-                    EMPIRE
-                </Text>
-                <Text family={"display"} weight={"2"} size={"7"} variant={"brown200"}>
-                    BURGUER
-                </Text>
-            </Flex>
+                <Flex
+                    justifyContent={"center"}
+                    css={{
+                        background: "var(--palete-black)",
+                        backdropFilter: "blur(5px)",
+                        border: "1px solid var(--palete-black)",
+                    }}
+                >
+                    <Flex css={{ gap: 7, marginRight: 61 }}>
+                        <img src={Crown} alt="CrownLogo" />
+                        <Text
+                            family={"display"}
+                            weight={"4"}
+                            size={"7"}
+                            variant={"brown200"}
+                        >
+                            EMPIRE
+                        </Text>
+                        <Text
+                            family={"display"}
+                            weight={"2"}
+                            size={"7"}
+                            variant={"brown200"}
+                        >
+                            BURGUER
+                        </Text>
+                    </Flex>
 
-            <Flex css={{ gap: 16, marginRight: 100 }}>
-                <Text as='a' family={"display"} weight={"2"} size={"3"} variant={"brown300"} href="http://pudim.com.br" >Home</Text>
-                <Text as='a' family={"display"} weight={"2"} size={"3"} variant={"brown300"} href="http://pudim.com.br" >Promoção</Text>
-                <Text as='a' family={"display"} weight={"2"} size={"3"} variant={"brown300"} href="http://pudim.com.br" >Cardapio</Text>
-                <Text as='a' family={"display"} weight={"2"} size={"3"} variant={"brown300"} href="http://pudim.com.br" >Comentario</Text>
-                <Text as='a' family={"display"} weight={"2"} size={"3"} variant={"brown300"} href="http://pudim.com.br" >Contato</Text>
-            </Flex>
-            <Flex css={{ gap: 16, marginRight: 13 }}>
-                <a href=""><img src={IfoodLogo} alt="ifood" color="black" /></a>
-                <a href=""> <img src={InstagramLogo} alt="instagram" color="black" /></a>
-                <Divider orientation={'vertical'} variant={'red'} css={{ height: 47 }} />
-            </Flex>
-            <StyledIconButton variant={'primary'} css={{ padding: '4px 16px' }}>
-                <img src={WhatsAppLogo} alt="whatsapp" />
-                Contato
-            </StyledIconButton>
-        </Flex>
+                    <Flex css={{ gap: 16, marginRight: 100 }}>
+                        <Text
+                            as="a"
+                            family={"display"}
+                            weight={"2"}
+                            size={"3"}
+                            variant={"brown300"}
+                            href="http://pudim.com.br"
+                        >
+                            Home
+                        </Text>
+                        <Text
+                            as="a"
+                            family={"display"}
+                            weight={"2"}
+                            size={"3"}
+                            variant={"brown300"}
+                            href="http://pudim.com.br"
+                        >
+                            Promoção
+                        </Text>
+                        <Text
+                            as="a"
+                            family={"display"}
+                            weight={"2"}
+                            size={"3"}
+                            variant={"brown300"}
+                            href="http://pudim.com.br"
+                        >
+                            Cardapio
+                        </Text>
+                        <Text
+                            as="a"
+                            family={"display"}
+                            weight={"2"}
+                            size={"3"}
+                            variant={"brown300"}
+                            href="http://pudim.com.br"
+                        >
+                            Comentario
+                        </Text>
+                        <Text
+                            as="a"
+                            family={"display"}
+                            weight={"2"}
+                            size={"3"}
+                            variant={"brown300"}
+                            href="http://pudim.com.br"
+                        >
+                            Contato
+                        </Text>
+                    </Flex>
+                    <Flex css={{ gap: 16, marginRight: 13 }}>
+                        <a href="">
+                            <img src={IfoodLogo} alt="ifood" color="black" />
+                        </a>
+                        <a href="">
+                            {" "}
+                            <img src={InstagramLogo} alt="instagram" color="black" />
+                        </a>
+                        <Divider
+                            orientation={"vertical"}
+                            variant={"red"}
+                            css={{ height: 47 }}
+                        />
+                    </Flex>
+                    <StyledIconButton variant={"primary"} css={{ padding: "4px 16px" }}>
+                        <img src={WhatsAppLogo} alt="whatsapp" />
+                        Contato
+                    </StyledIconButton>
+                </Flex>
+            </div>
+        </>
     );
 };
--- src/lib/tokens/colors.ts
+++ src/lib/tokens/colors.ts
@@ -1,6 +1,7 @@
 export const palette = {
   "--palette-white": "rgba(255, 255, 255, 0.9)",
   "--palette-bege": "rgba(250, 228, 208, 1)",
+  "--palete-black": "rgba(59, 32, 11, 0.05)",
   "--palette-gray": "rgba(73, 46, 21, 0.15)",
   "--palette-red-100": "rgba(244, 49, 39, 1)",
   "--palette-red-200": "rgba(181, 11, 4, 1)",
